ca146e1d00aae4cb2a55f5d2948bca834d8c4427
Mapphie <99549445+Mapphie@users.noreply.github.com>
2025-03-10 23:02:23 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
 import { AngularAppEngine, createRequestHandler } from '@angular/ssr'
-  import { getContext } from '@netlify/angular-runtime/context'
+import { getContext } from '@netlify/angular-runtime/context'
 ​
-  const angularAppEngine = new AngularAppEngine()
+const angularAppEngine = new AngularAppEngine()
 ​
-  export async function netlifyAppEngineHandler(request: Request): Promise<Response> {
-    const context = getContext()
+export async function netlifyAppEngineHandler(request: Request): Promise<Response> {
+  const context = getContext()
 ​
-    const result = await angularAppEngine.handle(request, context)
-    return result || new Response('Not found', { status: 404 })
-  }
+  const result = await angularAppEngine.handle(request, context)
+  return result || new Response('Not found', { status: 404 })
+}
 ​
   /**
    * The request handler used by the Angular CLI (dev-server and during build).
    */
-  export const reqHandler = createRequestHandler(netlifyAppEngineHandler)
+export const reqHandler = createRequestHandler(netlifyAppEngineHandler)
