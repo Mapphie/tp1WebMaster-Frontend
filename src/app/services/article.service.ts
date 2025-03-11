f85acca95134f46920ee27d8ad49305414525b72
Mapphie <maphiesarobidyrazafindrakotoso@gmail.com>
2025-03-11 15:35:57 +0300

    naverina hatramy voalohany

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticleService {
-  private apiUrl = 'http://127.0.0.1:5000/articles';
+  private apiUrl = environment.url;
   constructor(private http: HttpClient) { }
 
   getArticles(): Observable<any>{
