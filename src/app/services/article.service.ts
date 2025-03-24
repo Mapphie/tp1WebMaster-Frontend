200f57671e2e8ef0468ab7199b47b5ebdbcc764c
Mapphie <99549445+Mapphie@users.noreply.github.com>
2025-03-24 11:19:41 +0300

    Update article.service.ts
    environments

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticleService {
-  private apiUrl = 'https://tp1webmaster-backend.onrender.com/articles';
+  private apiUrl = environment.url;
   constructor(private http: HttpClient) { }
 
   getArticles(): Observable<any>{
